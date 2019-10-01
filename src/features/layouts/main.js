4060e1fe7fe54ddabc5631acb35eff0851a153dc
Alejandro Valladares <alejo.valladares14@gmail.com>
2019-10-01 05:11:31 -0600

    Change layout to a simpler one

--- src/features/layouts/main.js
+++ src/features/layouts/main.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
-import logo from '../../assets/logo.png'
-const { SubMenu } = Menu;
-const { Header, Content, Sider } = Layout;
+import { Layout, Menu, Breadcrumb,  } from 'antd';
+import logo from '../../assets/logo.png';
+const { Header, Content, Footer } = Layout;
 
 function App() {
 	return (
 		<Layout>
-			<Header className="header">
+			<Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
 				<img className="logo" src={logo} alt="logo" />
 				<Menu theme="dark" mode="horizontal" defaultSelectedKeys={[ '2' ]} style={{ lineHeight: '64px' }}>
 					<Menu.Item key="1">nav 1</Menu.Item>
@@ -16,76 +15,11 @@ function App() {
 					<Menu.Item key="3">nav 3</Menu.Item>
 				</Menu>
 			</Header>
-			<Layout>
-				<Sider width={200} style={{ background: '#fff' }}>
-					<Menu
-						mode="inline"
-						defaultSelectedKeys={[ '1' ]}
-						defaultOpenKeys={[ 'sub1' ]}
-						style={{ height: '100%', borderRight: 0 }}
-					>
-						<SubMenu
-							key="sub1"
-							title={
-								<span>
-									<Icon type="user" />
-									subnav 1
-								</span>
-							}
-						>
-							<Menu.Item key="1">option1</Menu.Item>
-							<Menu.Item key="2">option2</Menu.Item>
-							<Menu.Item key="3">option3</Menu.Item>
-							<Menu.Item key="4">option4</Menu.Item>
-						</SubMenu>
-						<SubMenu
-							key="sub2"
-							title={
-								<span>
-									<Icon type="laptop" />
-									subnav 2
-								</span>
-							}
-						>
-							<Menu.Item key="5">option5</Menu.Item>
-							<Menu.Item key="6">option6</Menu.Item>
-							<Menu.Item key="7">option7</Menu.Item>
-							<Menu.Item key="8">option8</Menu.Item>
-						</SubMenu>
-						<SubMenu
-							key="sub3"
-							title={
-								<span>
-									<Icon type="notification" />
-									subnav 3
-								</span>
-							}
-						>
-							<Menu.Item key="9">option9</Menu.Item>
-							<Menu.Item key="10">option10</Menu.Item>
-							<Menu.Item key="11">option11</Menu.Item>
-							<Menu.Item key="12">option12</Menu.Item>
-						</SubMenu>
-					</Menu>
-				</Sider>
-				<Layout style={{ padding: '0 24px 24px' }}>
-					<Breadcrumb style={{ margin: '16px 0' }}>
-						<Breadcrumb.Item>Home</Breadcrumb.Item>
-						<Breadcrumb.Item>List</Breadcrumb.Item>
-						<Breadcrumb.Item>App</Breadcrumb.Item>
-					</Breadcrumb>
-					<Content
-						style={{
-							background: '#fff',
-							padding: 24,
-							margin: 0,
-							minHeight: 280
-						}}
-					>
-						Content
-					</Content>
-				</Layout>
-			</Layout>
+			<Content style={{ padding: '0 50px', marginTop: 100 }}>
+				
+				<div style={{ background: '#fff', padding: 24, minHeight: 380 }}>Content</div>
+			</Content>
+			<Footer style={{ textAlign: 'center' }}>Alejandro Valladares 2019</Footer>
 		</Layout>
 	);
 }
