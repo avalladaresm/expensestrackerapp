3885ed199e4c9c1131632f97838b92636d16bfe8
Alejandro Valladares <alejo.valladares14@gmail.com>
2019-10-02 08:53:33 -0600

    Add expense form complete

--- src/features/expenses/NewExpense.js
+++ src/features/expenses/NewExpense.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Modal, Form, Input } from 'antd';
+import { Modal, Form, Input, Row, Col, Select, DatePicker } from 'antd';
+const { TextArea } = Input;
 
 class NewExpense extends React.Component {
 	constructor(props) {
@@ -25,7 +26,36 @@ class NewExpense extends React.Component {
 		return (
 			<Modal visible={visible} title="Add expense" okText="Add" onCancel={onCancel} onOk={this.onOk}>
 				<Form layout="vertical">
-					<Form.Item label="Amount">{getFieldDecorator('amount')(<Input />)}</Form.Item>
+					<Row gutter={16}>
+						<Col span={6}>
+							<Form.Item label="Amount">{getFieldDecorator('amount')(<Input allowClear />)}</Form.Item>
+						</Col>
+						<Col span={18}>
+							<Form.Item label="Place">{getFieldDecorator('place')(<Input allowClear />)}</Form.Item>
+						</Col>
+					</Row>
+					<Row gutter={16}>
+						<Col span={12}>
+							<Form.Item label="Category">
+								{getFieldDecorator('category')(<Select />)}
+							</Form.Item>
+						</Col>
+						<Col span={12}>
+							<Form.Item label="Payment type">
+								{getFieldDecorator('paymentType')(<Input allowClear />)}
+							</Form.Item>
+						</Col>
+					</Row>
+					<Row gutter={16}>
+						<Col span={24}>
+							<Form.Item label="Description">{getFieldDecorator('description')(<TextArea />)}</Form.Item>
+						</Col>
+					</Row>
+					<Row gutter={16}>
+						<Col span={24}>
+							<Form.Item label="Date">{getFieldDecorator('date')(<DatePicker />)}</Form.Item>
+						</Col>
+					</Row>
 				</Form>
 			</Modal>
 		);
