6e3a1f40b5e050013265b487468a9cee81f9db60
Alejandro Valladares <alejo.valladares14@gmail.com>
2019-10-02 07:24:31 -0600

    New expense modal working

--- src/features/expenses/Expenses.js
+++ src/features/expenses/Expenses.js
@@ -2,15 +2,33 @@ import React from 'react';
 import { Card, Button } from 'antd';
 import NewExpense from './NewExpense';
 
-function Expenses() {
-	return (
-		<Card title="Expenses" style={{ width: 300, display: 'inline-block', margin: '0 20px 0 20px' }}>
-			<Button type="primary">
-				Add Expense
-			</Button>
-			<NewExpense />
-		</Card>
-	);
+class Expenses extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			visible: false
+		};
+	}
+
+	showModal = () => {
+		this.setState({ visible: true });
+	};
+
+	onCancel = () => {
+		this.setState({ visible: false });
+	};
+
+	render() {
+		let { visible } = this.state;
+		return (
+			<Card title="Expenses" style={{ width: 300, display: 'inline-block', margin: '0 20px 0 20px' }}>
+				<Button type="primary" onClick={this.showModal}>
+					Add Expense
+				</Button>
+				<NewExpense visible={visible} onCancel={this.onCancel} />
+			</Card>
+		);
+	}
 }
 
 export default Expenses;
--- src/features/expenses/NewExpense.js
+++ src/features/expenses/NewExpense.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { Modal } from 'antd';
+import { Modal, Form, Input } from 'antd';
 
 class NewExpense extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {};
-    }
-    
+	}
+
 	render() {
-		const { visible, onCancel, onOk } = this.props;
+		const { visible, onCancel } = this.props;
+
 		return (
-			<Modal
-				visible={visible}
-				title="Add expense"
-				okText="Add"
-				onCancel={onCancel}
-				onOk={onOk}
-			>
+			<Modal visible={visible} title="Add expense" okText="Add" onCancel={onCancel}>
+				<Form layout="vertical">
+					<Form.Item label="Amount">
+						<Input />
+					</Form.Item>
+				</Form>
 			</Modal>
 		);
 	}
