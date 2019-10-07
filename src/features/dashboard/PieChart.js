0bfb22014f91149d0d38407b940cc81795df8d85
Alejandro Valladares <alejo.valladares14@gmail.com>
2019-10-07 12:13:36 -0600

    Add total expense label to pie chart

--- src/features/dashboard/PieChart.js
+++ src/features/dashboard/PieChart.js
@@ -13,39 +13,46 @@ class LastRecordsTimeline extends React.Component {
 
 	render() {
 		let { expensesByCategory } = this.props;
+		let totals = expensesByCategory.map((expense) => expense.total);
+		let total = totals.length != 0 ? totals.reduce((acc, curr) => acc + curr) : '';
 
 		return (
 			<Card title="Expenses by category" className="pieChartCard">
 				<Mayre
 					of={
-						<PieChart width={300} height={250}>
-							<Pie
-								dataKey="total"
-								nameKey="category"
-								isAnimationActive={false}
-								data={expensesByCategory}
-								cx={125}
-								cy={125}
-								outerRadius={80}
-								fill="#8884d8"
-								label
-							>
-								{expensesByCategory.map((entry, index) => <Cell key={index} fill={colors[index]} />)}
-							</Pie>
-							<Tooltip />
-							<Legend
-								margin={{
-									top: 100,
-									right: 100,
-									left: 100,
-									bottom: 100
-								}}
-								align="right"
-								layout="vertical"
-								iconType="circle"
-								iconSize={10}
-							/>
-						</PieChart>
+						<div>
+							{<h3>Total expenses: Lps. {total}</h3>}{' '}
+							<PieChart width={300} height={250}>
+								<Pie
+									dataKey="total"
+									nameKey="category"
+									isAnimationActive={false}
+									data={expensesByCategory}
+									cx={125}
+									cy={125}
+									outerRadius={80}
+									fill="#8884d8"
+									label
+								>
+									{expensesByCategory.map((entry, index) => (
+										<Cell key={index} fill={colors[index]} />
+									))}
+								</Pie>
+								<Tooltip />
+								<Legend
+									margin={{
+										top: 100,
+										right: 100,
+										left: 100,
+										bottom: 100
+									}}
+									align="right"
+									layout="vertical"
+									iconType="circle"
+									iconSize={10}
+								/>
+							</PieChart>
+						</div>
 					}
 					or={<Empty />}
 					when={expensesByCategory.length > 0}
