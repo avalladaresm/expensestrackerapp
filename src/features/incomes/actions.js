5fc67825d07651816e73bd543cab97957824eb63
Alejandro Valladares <alejo.valladares14@gmail.com>
2019-10-08 07:55:55 -0600

    Change localhost for expensestrackerapi site for api calls

--- src/features/incomes/actions.js
+++ src/features/incomes/actions.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 import { GET_INCOMES, ADD_INCOME } from './actionTypes';
 
 export const GetIncomes = () => (dispatch) => {
-	return axios.get(`http://localhost:4000/api/Incomes/`).then((response) => {
+	return axios.get(`https://expensestrackerapi.herokuapp.com/api/Incomes/`).then((response) => {
 		dispatch({ type: GET_INCOMES, payload: response.data });
 	});
 };
 
 export const AddIncome = (data) => (dispatch) => {
-	return axios.post(`http://localhost:4000/api/Incomes/`, data).then((response) => {
+	return axios.post(`https://expensestrackerapi.herokuapp.com/api/Incomes/`, data).then((response) => {
 		dispatch({ type: ADD_INCOME, payload: response.data });
 	});
 };
--- src/features/layouts/actions.js
+++ src/features/layouts/actions.js
@@ -4,25 +4,25 @@ import { GET_EXPENSES } from '../expenses/actionTypes';
 import { GET_LAST_RECORDS, GET_EXPENSES_BY_CATEGORY } from '../dashboard/actionTypes';
 
 export const InitExpenses = () => (dispatch) => {
-	return axios.get(`http://localhost:4000/api/Expenses/`).then((response) => {
+	return axios.get(`https://expensestrackerapi.herokuapp.com/api/Expenses/`).then((response) => {
 		dispatch({ type: GET_EXPENSES, payload: response.data });
 	});
 };
 
 export const InitIncomes = () => (dispatch) => {
-	return axios.get(`http://localhost:4000/api/Incomes/`).then((response) => {
+	return axios.get(`https://expensestrackerapi.herokuapp.com/api/Incomes/`).then((response) => {
 		dispatch({ type: GET_INCOMES, payload: response.data });
 	});
 };
 
 export const InitLastRecords = () => (dispatch) => {
-	return axios.get(`http://localhost:4000/api/Dashboards/getLastRecords`).then((response) => {
+	return axios.get(`https://expensestrackerapi.herokuapp.com/api/Dashboards/getLastRecords`).then((response) => {
 		dispatch({ type: GET_LAST_RECORDS, payload: response.data });
 	});
 };
 
 export const InitExpensesByCategory = () => (dispatch) => {
-	return axios.get(`http://localhost:4000/api/Dashboards/getExpensesByCategory`).then((response) => {
+	return axios.get(`https://expensestrackerapi.herokuapp.com/api/Dashboards/getExpensesByCategory`).then((response) => {
 		dispatch({ type: GET_EXPENSES_BY_CATEGORY, payload: response.data });
 	});
 };
